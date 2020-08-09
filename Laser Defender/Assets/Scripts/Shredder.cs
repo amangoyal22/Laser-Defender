ed0d15be411e3bf23fbc74f93e3335bf7bd1e10d
amangoyal <aman.goyalwd@gmail.com>
2020-08-10 00:48:38 +0530

    path way

---
+++ Laser Defender/Assets/Scripts/Shredder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D other)
+    {   
+        Debug.Log("triggred");
+        Destroy(other.gameObject);
+    }
+}
